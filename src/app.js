12f79cf29f2641813864efe0506cc1d15d7e4eb0
thianecw <thianecw@hotmail.com>
2025-02-14 08:14:52 -0300

    base de dados

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
 import express from "express";
 import { resolve } from "node:path";
 import routes from "./routes";
+import cors from "cors";
 
 import "./database";
 
 class App {
-  constructor() {
-    this.app = express();
+	constructor() {
+		this.app = express();
 
-    this.middlewares();
-    this.routes();
-  }
+		this.app.use(cors());
+		this.middlewares();
+		this.routes();
+	}
 
-  middlewares() {
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true })); // Adicionado!
-    this.app.use(
-      "/product-file",
-      express.static(resolve(__dirname, "..", "uploads"))
-    );
+	middlewares() {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true })); // Adicionado!
+		this.app.use(
+			"/product-file",
+			express.static(resolve(__dirname, "..", "uploads")),
+		);
 
-    this.app.use(
-      "./category-file",
-      express.static(resolve(__dirname, "..", "uploads"))
-    );
-  }
+		this.app.use(
+			"./category-file",
+			express.static(resolve(__dirname, "..", "uploads")),
+		);
+	}
 
-  routes() {
-    this.app.use(routes);
-  }
+	routes() {
+		this.app.use(routes);
+	}
 }
 
 export default new App().app;
